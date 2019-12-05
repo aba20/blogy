1cfc77cf5afe42b19de497a702cb3b8f24277637
aba20 <askr1000@gmail.com>
2019-12-05 12:01:20 +0300

    Add index route

--- server.js
+++ server.js
@@ -3,12 +3,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+
+// Require Route Files
+const indexRouter = require('./app/routes/index');
+
+
+
 // Instantiate Express Application Object
 const app = express();
 
-
 // Define PORT for the API to run on
 const port = process.env.PORT || 5000;
+
+
+
+//          ****  Routes  ****
+
+// Mount imoprted Routers
+app.use(indexRouter);
+
 
 
 // Start the server to listen for requests on a given port
